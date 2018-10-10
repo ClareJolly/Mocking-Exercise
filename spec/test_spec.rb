d21ba3073ab1416eee13c57d984a7ba74c0fcead
Clare Jolly <clarejollyuk@gmail.com>
2018-10-10 11:29:59 +0100

    test spec updated with doubles

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,12 +2,18 @@ require 'test'
 require 'answer'
 
 describe Test do
+  let(:answer)   {double :answer, :answer_given => 5, :correct_answer => 5, :correct? => true}
+  let(:answerB)   {double :answer, :answer_given => 7, :correct_answer => 7, :correct? => true}
+  let(:answerC)   {double :answer, :answer_given => 1, :correct_answer => 1, :correct? => true}
+
   let(:answers) do
     [
-      Answer.new(5, 5),
-      Answer.new(7, 7),
-      Answer.new(1, 1)
+      answer, answerB, answerC
+      # Answer.new(5, 5),
+      # Answer.new(7, 7),
+      # Answer.new(1, 1)
     ]
+
   end
 
   subject(:test) { Test.new(Date.today, answers) }
